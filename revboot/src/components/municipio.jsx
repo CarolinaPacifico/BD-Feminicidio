40c69af253ee55b3a24daa3679181149432c009f
CarolinaPacifico <84384263+CarolinaPacifico@users.noreply.github.com>
2023-06-21 12:52:53 -0300

    Add files via upload

--- revboot/src/components/municipio.jsx
+++ revboot/src/components/municipio.jsx
@@ -1,69 +1,10 @@
-import Table from 'react-bootstrap/Table';
+import municipio from '../img/municipio.svg'
 
 function TabelaMunicipio() {
   return (
-    <Table class="table table-striped table-bordered">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>MUNICIPIO_CIRCUNSCRICAO</th>
-          <th>QTDE</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>São Paulo</td>
-          <td>232</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Campinas</td>
-          <td>50</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>Guarulhos</td>
-          <td>28</td>
-        </tr>
-        <tr>
-          <td>4</td>
-          <td>Osasco</td>
-          <td>22</td>
-        </tr>
-        <tr>
-          <td>5</td>
-          <td>Sorocaba</td>
-          <td>18</td>
-        </tr>
-        <tr>
-          <td>6</td>
-          <td>Ribeirão Preto</td>
-          <td>14</td>
-        </tr>
-        <tr>
-          <td>7</td>
-          <td>Bauru</td>
-          <td>13</td>
-        </tr>
-        <tr>
-          <td>8</td>
-          <td>Santo André</td>
-          <td>12</td>
-        </tr>
-        <tr>
-          <td>9</td>
-          <td>São Bernardo do Campo</td>
-          <td>12</td>
-        </tr>
-        <tr>
-          <td>10</td>
-          <td>São José dos Campos</td>
-          <td>12</td>
-        </tr>
-        
-      </tbody>
-    </Table>
+      <div>
+      <img className='grafico' src={municipio} alt="Gráfico de Mês" width="50%" />
+    </div>
   );
 }
 
--- revboot/src/pages/Home.jsx
+++ revboot/src/pages/Home.jsx
@@ -21,18 +21,29 @@ function Home() {
           <h1>Dados Informativos sobre os casos de feminicídio no Brasil entre 2015 e 2022</h1><br/>
           <h2>Os 10 municípios brasileiros com maior número de casos de feminicídio relatados</h2>
           <TabelaMunicipio/>
+          <br />
+          <li>A cidade de São Paulo é onde o crime de feminicídio tem mais ocorrência</li>
           <br/>
           <h2>As 10 principais idades em que as mulheres sofreram feminicídio</h2>
           <TabelaIdade/>
+          <br />
+          <li>Mulheres entre 22 e 35 anos são as mais atingidas por esse crime</li>
           <br/>
           <h2>As 5 principais profissões em que as mulheres foram mais atingidas</h2>
           <TabelaProfissao/>
+          <br />
+          <li>Na maioria das vítimas, a profissão delas não pode ser identificada pelas autoridades.</li>
           <br/>
           <h2>Os 10 principais horários em que esse crime foi cometido</h2>
           <TabelaHora/>
+          <br />
+          <li>Os casos de feminicídio possuem uma maior incidência durante a madrugada.</li>
           <br/>
           <h2>O número de casos de feminicídio durante os meses</h2>
           <TabelaMes/>
+          <br />
+          <li>O mês de Dezembro é onde ocorre um maior número de feminicídios.</li>
+          <br />
           <Footer/>
         </div>
       </>
